2af320aa6f06bb2b475014ba5cc1cdbded7fbf86
sabaka-chabaka <gamekimg360@gmail.com>
2026-04-21 21:26:21 +0400

    Initial commit

---
+++ SabakaLib.UnitTests/EnumerableExtensionsTests.cs
@@ -0,0 +1,40 @@
+using Xunit;
+
+namespace SabakaLib.UnitTests;
+
+public class EnumerableExtensionsTests
+{
+    [Fact]
+    public void PickRandom_ReturnsDifferentElementsOverTime()
+    {
+        var source = Enumerable.Range(1, 1000).ToList();
+        var results = new HashSet<int>();
+
+        for (int i = 0; i < 10; i++)
+        {
+            results.Add(source.PickRandom());
+        }
+
+        Assert.True(results.Count > 1); 
+    }
+    
+    [Fact]
+    public void WhereNotNull_ReturnsNotNullElements()
+    {
+        var source = new List<string?> { "a", null, "b" };
+        var results = source.WhereNotNull();
+        
+        Assert.DoesNotContain(results, x => x == null);
+    }
+    
+    [Fact]
+    public void ForEach_CallsActionForEachElement()
+    {
+        var source = new List<int> { 1, 2, 3 };
+        var results = new List<int>();
+        
+        source.ForEach(results.Add);
+        
+        Assert.Equal(source, results);
+    }
+}
---
+++ SabakaLib/EnumerableExtensions.cs
@@ -0,0 +1,23 @@
+namespace SabakaLib;
+
+public static class EnumerableExtensions 
+{
+    public static T PickRandom<T>(this IEnumerable<T> source)
+    {
+        var list = source as IList<T> ?? source.ToList();
+        if (list.Count == 0) throw new InvalidOperationException("The source sequence is empty.");
+
+        var random = new Random();
+        return list[random.Next(list.Count)];
+    }
+    
+    public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> source) where T : class => source.Where(x => x != null)!;
+    
+    public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+    {
+        foreach (var item in source)
+        {
+            action(item);
+        }
+    }
+}
